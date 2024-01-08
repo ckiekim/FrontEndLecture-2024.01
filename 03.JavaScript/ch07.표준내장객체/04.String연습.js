fd7fc38c294b100fcc675f828ed6569ac4a39bfd
Cheolkie Kim <ckiekim@naver.com>
2024-01-08 15:45:19 +0900

    JS - 연습문제

--- 03.JavaScript/ch07.표준내장객체/04.String연습.js
+++ 03.JavaScript/ch07.표준내장객체/04.String연습.js
@@ -23,3 +23,15 @@ for (let i = 0; i < numStr.length; i++) {
     countArr[num]++;
 }
 console.log(countArr);
+
+// 2. 디지털 시계, 00:00 ~ 23:59
+// 하루동안 3이 표시되는 시간은 몇초인가?
+
+
+// 3. 두개의 세자리수를 곱해서 나온 결과가 palindrome일때
+// 가장 큰 palindrome 수와 어떤 수를 곱해서 나온 결과인가?
+
+
+// 4. C:/Workspace/WebProject/03.JavaScript/ch07.표준내장객체/04.String연습.js
+// 에서 파일명(04.String연습.js)만 출력하세요.
+
